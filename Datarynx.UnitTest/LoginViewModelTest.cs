72b9509a2d4f4f5e2490578e69ea4dc9c13d45ef
91986 <91986@LAPTOP-KB18ES8T>
2021-05-14 00:02:47 +0530

    added fle

---
+++ Datarynx.UnitTest/LoginViewModelTest.cs
@@ -0,0 +1,26 @@
+﻿using Datarynx.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Datarynx.UnitTest
+{
+
+   public class LoginViewModelTest
+    {
+        private LoginViewModel _loginViewModel;
+        public LoginViewModelTest()
+        {
+            _loginViewModel = new LoginViewModel();
+        }
+
+        [Fact]
+        public void LoadItemsCommand_Not_Null_Test()
+        {
+
+            Assert.NotNull(_loginViewModel.LoginCommand);
+        }
+
+    }
+}
