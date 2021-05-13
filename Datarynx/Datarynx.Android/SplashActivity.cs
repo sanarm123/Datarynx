77494c0c41e995b0bdfb79d562fd91ebf494b4fe
91986 <91986@LAPTOP-KB18ES8T>
2021-05-13 16:32:24 +0530

    COMMIT

---
+++ Datarynx/Datarynx.Android/SplashActivity.cs
@@ -0,0 +1,45 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Datarynx.Droid
+{
+
+    [Activity(Label = "Datarynx", Icon = "@mipmap/icon", Theme = "@style/Mytheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    public class SplashActivity : Activity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+
+            try
+            {
+
+                var intent = new Intent(this, typeof(MainActivity));
+
+                if (Intent.Extras != null)
+                {
+                    intent.PutExtras(Intent.Extras);
+                }
+
+                StartActivity(intent);
+            }
+            catch (Exception ex)
+            {
+
+                System.Diagnostics.Debug.WriteLine(ex);
+
+            }
+        }
+    }
+}
