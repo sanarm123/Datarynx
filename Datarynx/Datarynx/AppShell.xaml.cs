b555c06bcf4c8c899cfcf6e421b963dd11a83314
91986 <91986@LAPTOP-KB18ES8T>
2021-05-18 21:19:22 +0530

    commit

--- Datarynx/Datarynx/AppShell.xaml.cs
+++ Datarynx/Datarynx/AppShell.xaml.cs
@@ -3,6 +3,8 @@ using Datarynx.Views;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace Datarynx
@@ -14,12 +16,26 @@ namespace Datarynx
         {
             InitializeComponent();
             Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
-        }
-      
 
+            OpenWebCommand = new Command(async () => await DelaMethod());
+
+            BindingContext = this;
+        }
+
+        private static async Task DelaMethod()
+        {
+            await Task.Delay(100);
+        }
+
+        public ICommand OpenWebCommand { get; }
         private async void OnMenuItemClicked(object sender, EventArgs e)
         {
             await Shell.Current.GoToAsync("//LoginPage");
+        }
+
+        private void HeaderContentView_Clicked(object sender, EventArgs e)
+        {
+            //
         }
     }
 }
--- Datarynx/Datarynx/Views/HeaderContentView.xaml.cs
+++ Datarynx/Datarynx/Views/HeaderContentView.xaml.cs
@@ -4,7 +4,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -14,9 +14,47 @@ namespace Datarynx.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class HeaderContentView : ContentView
 	{
+		public event EventHandler Clicked = delegate { };
+
+		public static readonly BindableProperty CommandProperty = BindableProperty.Create(nameof(Command), typeof(ICommand), typeof(HeaderContentView), propertyChanged: CommandUpdated);
+
+		public static readonly BindableProperty CommandParameterProperty = BindableProperty.Create(nameof(CommandParameter), typeof(object), typeof(HeaderContentView), propertyChanged: CommandParameterUpdated);
+		private static void CommandParameterUpdated(object sender, object oldValue, object newValue)
+		{
+			if (sender is HeaderContentView activityButton && newValue != null)
+			{
+				activityButton.innerButton.CommandParameter = newValue;
+			}
+		}
+		private static void CommandUpdated(object sender, object oldValue, object newValue)
+		{
+			if (sender is HeaderContentView activityButton && newValue is ICommand newCommand)
+			{
+				activityButton.innerButton.Command = newCommand;
+			}
+		}
+		public ICommand Command
+		{
+			get => (ICommand)this.GetValue(CommandProperty);
+			set => this.SetValue(CommandProperty, value);
+		}
+
+		public object CommandParameter
+		{
+			get => this.GetValue(CommandParameterProperty);
+			set => this.SetValue(CommandParameterProperty, value);
+		}
 		public HeaderContentView ()
 		{
 			InitializeComponent ();
+			this.innerButton.BindingContext = this;
+			this.innerButton.Clicked += this.OnClicked;
+		}
+
+		private void OnClicked(object sender, EventArgs args)
+		{
+			this.Clicked?.Invoke(sender, args);
+
 		}
 	}
 }
